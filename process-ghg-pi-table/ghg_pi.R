2f233f40ea623186ca68e144f17e59b1212984a0
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-07-25 14:47:28 -0700

    cleaning table

--- process-ghg-pi-table/ghg_pi.R
+++ process-ghg-pi-table/ghg_pi.R
@@ -10,24 +10,56 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
 library(readxl)
+library(dplyr)
+library(readr)
 
 
 ## Import the .xlsx table from data/
 dir <- "process-ghg-pi-table/data"
-filename <- "2015_provinicial_inventory.xlsx"
+filename <- "2015_provincial_inventory.xlsx"
 
-## First get the data, without the headers (2 rows)
-all_data <- read_xlsx(file.path(dir, filename), col_names = FALSE, skip = 2)
 
-## First row (header) contains the main questions. Use ncol of all_data to get
-## the cell range
-colnames_1 <- read_xlsx(file.path(dir, filename), col_names = FALSE,
-                        range = cell_limits(c(1,1), c(1, ncol(all_data)))) %>%
-  unlist()
+## Get the metadata
+units <- read_xlsx(file.path(dir, filename),
+                   col_names = c("Note", "blank1", "blank2", "Comment"),
+                   range = cell_rows(6)) %>%
+  select(Note, Comment)
+
+metadata <- read_xlsx(file.path(dir, filename),
+                      col_names = c("Note","Comment"),
+                      range = cell_rows(86:92)) %>%
+  rbind(units)
+
+
+## Get the column names
+newcols <- colnames(read_xlsx(file.path(dir, filename),
+                              col_names = TRUE, skip = 1))
+
+## Get the core data, without the title, empty leading rows & metadata, & add new column names
+data <- read_xlsx(file.path(dir, filename),
+                  col_names = newcols,
+                  skip = 6, n_max = 76) %>%
+    rename(subsector_level2 = "..2",
+         subsector_level3 = "..3") %>%
+  select(-`Greenhouse Gas Categories`) %>%
+  select(subsector_level2, subsector_level3, everything())
+
+## Filter if NA present in both subsector_level2 & subsector_level3
+## Filter subsector_level1 from subsector_level2
+
+## Get the emission categories for joining
+catfile <- "emission_categories.csv"
+cats <- read_csv(file.path(dir, catfile))
+
+
+
+
+
 
 ## Save the re-formatted data as CSV file
-write_csv(data, (file.path(dir, paste0(data_year, "_bc_ghg_emissions.csv"))))
-write_csv(metadata, (file.path(dir, paste0(data_year, "bc_ghg_emissions_metadata.csv"))))
+# write_csv(data_wide, (file.path(dir, paste0(data_year, "_bc_ghg_emissions.csv"))))
+# write_csv(metadata, (file.path(dir, paste0(data_year, "bc_ghg_emissions_metadata.csv"))))
 
 
