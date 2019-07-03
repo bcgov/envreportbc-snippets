4fafffb4c0e63a2c227e29b9f6711c02f5597e7e
Andy Teucher <andy.teucher@gmail.com>
2019-07-03 11:12:25 -0700

    Update carip script

--- process-carip-survey-data/carip.R
+++ process-carip-survey-data/carip.R
@@ -11,33 +11,27 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 library(tidyverse)
-library(readxl)
 
-## Set survey data year
-data_year <- "2017"
-
-## Import the raw data .xlsx file from data/
+## Use the csv file from Survey Monkey XLS export of 'All Individual Responses' - condensed
 dir <- "process-carip-survey-data/data"
-filename <- paste0(data_year, " CARIP raw data file.xlsx")
+filename <- "2018 CARIP Climate ActionCarbon Neutral Progress Survey.csv"
+file_path <- file.path(dir, filename)
+data_year <- "2018"
 
-## First get the data, without the headers (2 rows)
-all_data <- read_xlsx(file.path(dir, filename), col_names = FALSE, skip = 2)
-
-## First row (header) contains the main questions. Use ncol of all_data to get
-## the cell range
-colnames_1 <- read_xlsx(file.path(dir, filename), col_names = FALSE,
-                        range = cell_limits(c(1,1), c(1, ncol(all_data)))) %>%
+## First row (header) contains the main questions
+colnames_1 <- read_csv(file_path, col_names = FALSE, n_max = 1) %>%
   unlist()
 
-## Second row contains the sub-questions/descriptions/options. Use ncol of
-## all_data to get the cell range
-colnames_2 <- read_xlsx(file.path(dir, filename), col_names = FALSE,
-                        range = cell_limits(c(2,1), c(2, ncol(all_data)))) %>%
+## Second row contains the sub-questions/descriptions/options
+colnames_2 <- read_csv(file_path, col_names = FALSE, n_max = 1, skip = 1) %>%
   unlist()
 
-## Drop off the metadata questions (1-5) and create an empty integer column
-## to store question numbers
-q_labels_df <- bind_cols(question_text = colnames_1, description = colnames_2) %>%
+## Read in the data without the first two rows of header info
+all_data <- read_csv(file_path, col_names = FALSE, skip = 2)
+
+## Drop off the metadata questions (1-5) and create an empty integer column to store question numbers
+q_labels_df <- bind_cols(question_text = colnames_1,
+                         description = colnames_2) %>%
   slice(24:n()) %>%
   mutate(q_num = NA_integer_)
 
@@ -56,10 +50,19 @@ for (r in seq_len(nrow(q_labels_df))[-1]) {
 
 ## Reconstruct the questions and subquestions
 q_labels_df <- group_by(q_labels_df, q_num) %>%
-  mutate(sub_q = if (n() > 1) formatC(1:n(), width = 4, flag = "0") else NA_character_,
-         sub_q = ifelse(description == "Other (please specify)", "other", sub_q),
-         question = ifelse(is.na(sub_q), q_num, paste(q_num, sub_q, sep = "_")),
-         question_text = ifelse(is.na(question_text), question_text[1], question_text))
+  mutate(
+    sub_q = if (n() > 1) {
+      formatC(1:n(), width = 4, flag = "0")
+    } else {
+      NA_character_
+    },
+    sub_q = ifelse(description == "Other (please specify)", "other",
+                   sub_q),
+    question = ifelse(is.na(sub_q), q_num,
+                      paste(q_num, sub_q, sep = "_")),
+    question_text = ifelse(is.na(question_text), question_text[1],
+                           question_text)
+  )
 
 ## Extract the metadata (respondent information) columns from the full dataset
 metadata <- all_data[, 1:23]
@@ -68,15 +71,17 @@ metadata <- all_data[, 1:23]
 names(metadata) <- c(colnames_1[1:14], colnames_2[15:23])
 names(metadata) <- gsub("\\s+", "_", names(metadata))
 
-## Extract the question responses plus the Respondent ID, Local_Govt, and Member_RD columns
-data <- select(all_data, 1, 10, 11, X__24:ncol(all_data)) %>%
-  set_names(c(names(metadata)[1], "Local_Govt", "Member_RD", q_labels_df$question))
+## Exctract the question responses plus the Respondent ID, Local_Govt, and Member_RD column
+data <- select(all_data, 1, 10, 11, X24:ncol(all_data)) %>%
+  set_names(c(names(metadata)[1], "Local_Govt", "Member_RD",
+              q_labels_df$question))
 
-## Gather the wide columns to long format, then join the question data to the responses
-data_long <- gather(data, question, response, q0006_0001:q0103) %>%
+## gather the wide columns to long format, then join the question data to the responses.
+data_long <- gather(data, key = "question", value = "response",
+                    starts_with("q0")) %>%
   left_join(q_labels_df, by = "question") %>%
   mutate(q_num = as.integer(substr(q_num, 2, 5)),
-         sub_q = ifelse(sub_q == "other" | is.na(sub_q), 0L, as.integer(sub_q))) %>%
+         sub_q = ifelse(sub_q == "other", 0L, as.integer(sub_q))) %>%
   group_by(Respondent_ID, q_num) %>%
   mutate(
     question_type = case_when(
@@ -84,16 +89,15 @@ data_long <- gather(data, question, response, q0006_0001:q0103) %>%
       n() > 1 & nchar(description) > 1 ~ "Multiple Choice (multi-answer)",
       n() == 1 & description == "Response" ~ "Multiple Choice (single-answer)",
       n() == 1 & description == "Open-Ended Response" ~ "Open-ended (single-answer)",
-      n() > 1 & grepl("^[123456789]$", description) & is.integer(sub_q) ~ "Open-ended (multi-answer)",
+      n() > 1 & grepl("^[1-9]$", description) & is.integer(sub_q) ~ "Open-ended (multi-answer)",
       TRUE ~ "Aaaargh"
     )
   ) %>%
-  select(Respondent_ID, Local_Govt, Member_RD, question, q_num, sub_q, question_type,
-         question_text, description, response) %>%
+  select(Respondent_ID, Local_Govt, Member_RD, question, q_num, sub_q,
+         question_type, question_text, description, response) %>%
   arrange(Respondent_ID, question)
 
-## Save the re-formatted data as CSV files
-write_csv(data_long, (file.path(dir, paste0(data_year, "_survey_monkey_data_long.csv"))))
-write_csv(metadata, (file.path(dir, paste0(data_year, "_survey_monkey_metadata.csv"))))
-
-
+write_csv(data_long,
+          file.path(dir, paste0(data_year, "survey_monkey_data_long.csv")))
+write_csv(metadata,
+          file.path(dir, paste0(data_year, "survey_monkey_metadata.csv")))
