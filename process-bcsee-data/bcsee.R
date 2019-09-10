1dbfa42c8d32e909f1cd06e400938b8116ecb005
Andy Teucher <andy.teucher@gmail.com>
2019-09-10 14:27:55 -0700

    add variable with year; split into three input files for 2018

--- process-bcsee-data/bcsee.R
+++ process-bcsee-data/bcsee.R
@@ -17,7 +17,6 @@ library(readxl) #load xlsx files
 library(dplyr) # data munging
 library(dataCompareR) # compare dataframes
 
-
 ## Load historical datset from the BC Data Catalogue, distributed under the
 ## Open Government Licence - British Columbia:
 ## https://catalogue.data.gov.bc.ca/dataset/d3651b8c-f560-48f7-a34e-26b0afc77d84
@@ -29,11 +28,12 @@ hist_bcsee_com <- read_csv("https://catalogue.data.gov.bc.ca/dataset/d3651b8c-f5
 hist_bcsee_pa <- read_csv("https://catalogue.data.gov.bc.ca/dataset/d3651b8c-f560-48f7-a34e-26b0afc77d84/resource/39aa3eb8-da10-49c5-8230-a3b5fd0006a9/download/bcseeplantsanimals.csv", col_types = cols(.default = col_character()), na = c("","NA"))
 
 ## Year of annual snapshot you are adding - update each time
-Add_Year <- "2017"
+Add_Year <- "2018"
 
 ## Load annual snapshot .xlsx data files from data/ folder & add Year
-annual_snapshot_com <- "RecentYear_Communities.xlsx"
-annual_snapshot_pa <- "RecentYear_Plants_Animals.xlsx"
+annual_snapshot_com <- paste0(Add_Year, "_Communities.xlsx")
+annual_snapshot_p <- paste0(Add_Year, "_Plants.xlsx")
+annual_snapshot_a <- paste0(Add_Year, "_Animals.xls")
 
 new_com <- read_excel(file.path("process-bcsee-data/data", annual_snapshot_com),
                       sheet = "bcsee_export", col_types = "text") %>%
