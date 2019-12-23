43a1994b3d62d9638158eadb061c49c10cd9c4e5
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-12-23 13:32:14 -0800

    consolidate temporal data , assign regions

--- process-groundwater-reporting-data/01_load.R
+++ process-groundwater-reporting-data/01_load.R
@@ -78,6 +78,8 @@ get_well_data_graded = function(sheet, range, report_date) {
 wdata_0718 <- get_well_data_graded(sheet = "July 2018", range = "B2:J219", report_date = "2018-07-01")
 wdata_0719 <- get_well_data_graded(sheet = "July 2019 ", range = "E2:M236", report_date = "2019-07-01")
 
+wdata <- bind_rows(wdata_0219, wdata_0718, wdata_0719)
+
 
 get_well_data = function(sheet, range, report_date) {
   tdata <- read_excel(wfile, sheet = sheet, range = range,
@@ -103,33 +105,53 @@ wdata_0717 <- get_well_data("July 2017", "B2:I207", "2017-07-01" )
 wdata_0217 <- get_well_data("Feb 2017", "B2:I198", "2017-02-01" )
 wdata_0716 <- get_well_data("July 2016", "B2:I192", "2016-07-01" )
 wdata_0316 <- get_well_data("March 2016", "B2:I193", "2016-03-01" )
-wdata_0715 <- get_well_data("July 2015", "B2:I193", "2015-07-01" )
-wdata_0215 <- get_well_data("Feb 2015", "B2:I168", "2015-02-01" )
+
+# remove 2015 datasets
+#wdata_0715 <- get_well_data("July 2015", "B2:I193", "2015-07-01" )
+#wdata_0215 <- get_well_data("Feb 2015", "B2:I168", "2015-02-01" )
+
+
+wdata <- bind_rows(wdata, wdata_0218, wdata_0717,wdata_0217,
+                   wdata_0716, wdata_0316)
+
+
+# update missing "region" values
+
+region_table <- tribble(
+  ~ Location, ~ Region2,
+  "Merrit", "Cariboo/Thompson",
+  "Joe Rich", "Okanagan/Kootenay",
+  "Pemberton", "Lower Mainland",
+  "Deroche", "Lower Mainland",
+  "Dewdney", "Lower Mainland",
+  "NG-Charlie Lake", "Ominca/Peace",
+  "Canoe Creek", "Cariboo/Thompson",
+  "Farmington", "Ominca/Peace",
+  "Junction Sheep", "Cariboo/Thompson",
+  "Shuswamp Lake Park Deep", "Cariboo/Thompson",
+  "Salmon Arm", "Cariboo/Thompson",
+  "Ellison", "Okanagan/Kootenay"
+)
+
+wdata <- wdata %>%
+  left_join(region_table) %>%
+  mutate(Region = ifelse(is.na(Region), Region2, Region)) %>%
+  select(- Region2)
+
+
+wdata <- wdata %>%
+  group_by(Location) %>%
+  mutate(Region = ifelse(is.na(Region), first(Region), Region )) %>%
+  ungroup()
+
+
+# data checks
+with.region <- wdata %>% filter(!is.na(Region))
+
+no.region <- wdata %>% filter(is.na(Region))
 
 
 
-
-bind_rows(wdata_0219,wdata_0215)
-
-# July 2018
-wdata_0718 <- read_excel(wfile, sheet = "July 2018", range = "A2:J219",
-                         col_names = c("Region", "Data_graded", "Well_ID", "Location",
-                                       "Date_Validated", "Months_since_val", "foo","initial_cost","foo1", "comment"),
-                         col_types = c("text", "text", "text","text", "date", "text",
-                                       "text", "text", "text","text")) %>%
-  select(-c("foo", "foo1")) %>%
-  mutate(Region = ifelse(str_detect(Region, "%"),NA , Region),
-         Region = ifelse(str_detect(Region, "Total"),NA ,Region),
-         initial_cost = as.numeric(initial_cost),
-         Well_ID = as.integer(gsub("^#", "", Well_ID))) %>%
-  fill(Region) %>%
-  filter_at(.vars = vars(Data_graded, Well_ID), .vars_predicate = any_vars(!is.na(.))) %>%
-  mutate(report_data = "2018-07-01",
-         dateCheck = round(interval(ymd(Date_Validated),
-                                    ymd("2018-07-01"))/ months(1), 0))
-
-
-bind_rows(wdata_0219,wdata_0718 )
 
 
 # import well dataset from the data catalogue ( )
@@ -144,16 +166,11 @@ wells <- bcdc_query_geodata("e4731a85-ffca-4112-8caf-cb0a96905778") %>%
          WELL_DETAIL_URL) %>%
   collect()
 
-wells_joined <- right_join(wells, wdata_0219,
+
+wells_joined <- right_join(wells, wdata ,
                           by = c("OBSERVATION_WELL_NUMBER" = "Well_ID"))
 
 library(mapview)
-
-# export the R objects.
-
-if (!dir.exists("tmp")) dir.create("tmp")
-
-save(list = ls(), file = "tmp/welldata.RData")
 
 bc <- bcmaps::bc_bound()
 
@@ -171,3 +188,12 @@ wells_regions <- st_union(wells_joined) %>%
 
 mapview(wells_regions, zcol = "Region") +
   mapview(wells_joined, zcol = "Region", legend = FALSE)
+
+
+
+# export the R objects.
+
+if (!dir.exists("tmp")) dir.create("tmp")
+
+save(list = ls(), file = "tmp/welldata.RData")
+
