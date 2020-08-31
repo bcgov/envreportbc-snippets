982bfa08cd2d72dc428940c7729bb45c162ad033
Andy Teucher <andy.teucher@gov.bc.ca>
2020-08-31 16:20:35 -0700

    Add econ sector pi and specify sheet in activity pi

--- process-ghg-pi-table/ghg_pi_econ-sector.R
+++ process-ghg-pi-table/ghg_pi_econ-sector.R
@@ -27,17 +27,17 @@ filename <- "bc_provincial_ghg_inventory_1990-2018.xlsx"
 ## Get the metadata from the sheet
 units <- read_xlsx(file.path(getwd(),dir, filename),
                    col_names = c("Notes"),
-                   range = "Activity Categories!B3")
+                   range = "Economic Sectors!B3")
 
 metadata <- read_xlsx(file.path(getwd(),dir, filename),
                       col_names = c("Notes"),
-                      range = "Activity Categories!B90:B99") %>%
+                      range = "Economic Sectors!B66:B71") %>%
   filter(!grepl("Indicates no emissions", Notes)) %>%
   rbind(units)
 
 ## Get the column names
 newcols <- c("all_sectors", colnames(read_xlsx(file.path(dir, filename),
-                                          col_names = TRUE, range = "Activity Categories!C3:AE3")))
+                                               col_names = TRUE, range = "Economic Sectors!C3:AE3")))
 
 ## Get the core data, wrangle the 3 attribute columns
 ## into the official sector & 3 subsector columns, and filter out total rows
@@ -46,39 +46,38 @@ newcols <- c("all_sectors", colnames(read_xlsx(file.path(dir, filename),
 formats <- xlsx_formats(file.path(dir, filename))
 
 sector_cell_formats <- xlsx_cells(file.path(dir, filename),
-           sheets = "Activity Categories",
-           include_blank_cells = FALSE) %>%
-  filter(col == 2, between(row, 5, 76) | between(row, 79, 88)) %>%
+                                  sheets = "Economic Sectors",
+                                  include_blank_cells = FALSE) %>%
+  filter(col == 2, between(row, 5, 51) | between(row, 54, 63)) %>%
   select(address, row, col, all_sectors = character, local_format_id) %>%
   mutate(
     all_sectors = gsub("^\\s+|\\s+$", "", all_sectors),
     text_colour = map_chr(local_format_id, ~ formats$local$font$color$rgb[[.x]]),
-         bg_colour = map_chr(local_format_id, ~ formats$local$fill$patternFill$bgColor$rgb[[.x]]),
-         bold = map_lgl(local_format_id, ~ formats$local$font$bold[[.x]]),
-         indent = map_int(local_format_id, ~ formats$local$alignment$indent[[.x]]),
-         sector_level = case_when(
-           is.na(text_colour) & bg_colour == "FFFFFFFF" & bold & indent == 0 ~ "sector",
-           text_colour == "FF00783C" & bold & indent == 0 ~ "subsector_level1",
-           !bold & indent == 1 ~ "subsector_level2",
-           text_colour == "FFFFFFFF" & !bold & indent == 3 ~ "subsector_level3",
-           TRUE ~ "ahhhh"
-         )
+    bg_colour = map_chr(local_format_id, ~ formats$local$fill$patternFill$bgColor$rgb[[.x]]),
+    bold = map_lgl(local_format_id, ~ formats$local$font$bold[[.x]]),
+    indent = map_int(local_format_id, ~ formats$local$alignment$indent[[.x]]),
+    sector_level = case_when(
+      text_colour == "FF00783C" & bold & indent == 0 ~ "sector",
+      !bold & indent == 1 ~ "subsector_level1",
+      text_colour == "FFFFFFFF" & !bold & (indent == 2 | indent ==3) ~ "subsector_level2",
+      TRUE ~ "ahhhh"
+    )
   )
 
 
 # Join sector level info with data, filter out total rows
 data_wide <- read_xlsx(file.path(dir, filename),
                        col_names = newcols,
-                       range = "Activity Categories!B5:AE76",
+                       range = "Economic Sectors!B5:AE51",
                        na = c("", "-")) %>%
   mutate(row = seq(5, length.out = nrow(.))) %>%
   bind_rows(
     read_xlsx(file.path(dir, filename),
-                      col_names = newcols,
-                      range = "Activity Categories!B79:AE88",
-                      na = c("", "-")) %>%
-      mutate(row = seq(79, length.out = nrow(.)))
-    ) %>%
+              col_names = newcols,
+              range = "Economic Sectors!B54:AE63",
+              na = c("", "-")) %>%
+      mutate(row = seq(54, length.out = nrow(.)))
+  ) %>%
   left_join(
     sector_cell_formats %>%
       select(row, all_sectors, sector_level),
@@ -88,23 +87,22 @@ data_wide <- read_xlsx(file.path(dir, filename),
   pivot_wider(names_from = sector_level, values_from = all_sectors) %>%
   mutate(
     subsector_level1 = ifelse(!is.na(sector), "total", subsector_level1),
-    subsector_level2 = ifelse(!is.na(subsector_level1), "total", subsector_level2),
-    subsector_level3 = ifelse(!is.na(subsector_level2), NA_character_, subsector_level3),
-    sector = ifelse(subsector_level1 == "OTHER LAND USE", "Other Emissions Not Included In Inventory Total", sector)
+    subsector_level2 = ifelse(!is.na(subsector_level1), NA_character_, subsector_level2),
+    sector = ifelse(sector == "OTHER LAND USE", "Other Emissions Not Included In Inventory Total", sector)
   ) %>%
-  fill(sector, subsector_level1, subsector_level2) %>%
-  filter(sector != "total" & subsector_level1 != "total" & subsector_level2 != "total") %>%
-  group_by(sector, subsector_level1, subsector_level2) %>%
+  fill(sector, subsector_level1) %>%
+  filter(sector != "total" & subsector_level1 != "total") %>%
+  group_by(sector, subsector_level1) %>%
   mutate(n = n()) %>%
   ungroup() %>%
-  filter(n == 1 | (!is.na(subsector_level3) & n > 1)) %>%
-  select(sector, subsector_level1, subsector_level2, subsector_level3, `1990`:`2018`)
+  filter(n == 1 | (!is.na(subsector_level2) & n > 1)) %>%
+  select(sector, subsector_level1, subsector_level2, `1990`:`2018`)
 
 ## Testing to make sure sums are same as input table
 data_long <- data_wide %>%
   gather(key =  year, value = ktCO2e,
          -sector, -subsector_level1,
-         -subsector_level2, -subsector_level3) %>%
+         -subsector_level2) %>%
   mutate(ktCO2e = as.numeric(ktCO2e),
          year = as.integer(as.character(year)))
 
@@ -125,5 +123,5 @@ sector_totals
 
 ## Save the re-formatted data as CSV file
 data_year <- "2018"
-write_csv(data_wide, (file.path(dir, paste0(data_year, "_bc_ghg_emissions_by_activity.csv"))))
-write_csv(metadata, (file.path(dir, paste0(data_year, "_bc_ghg_emissions_metadata_by_activity.csv"))))
+write_csv(data_wide, (file.path(dir, paste0(data_year, "_bc_ghg_emissions_by_economic_sector.csv"))))
+write_csv(metadata, (file.path(dir, paste0(data_year, "_bc_ghg_emissions_by_economic_sector_metadata.csv"))))
