428a0bbae2fa9fe08e8594a912f6b3ffdb70734e
Andrew Ladle <andrew.ladle@gov.bc.ca>
2023-09-15 16:34:32 -0700

    Filter both columns 2 and 3 to get gas name (column 2) and sector(column 3), then use ipcc criteria to delineate gas, sector, subsector etc

--- process-ghg-pi-table/ghg_pi_gases.R
+++ process-ghg-pi-table/ghg_pi_gases.R
@@ -36,7 +36,7 @@ metadata <- read_xlsx(file.path(getwd(),dir, filename),
   rbind(units)
 
 ## Get the column names
-newcols <- c("all_sectors", colnames(read_xlsx(file.path(dir, filename),
+newcols <- c("gas","all_sectors", colnames(read_xlsx(file.path(dir, filename),
                                                col_names = TRUE, range = "Gases!D3:AI3")))
 
 
@@ -46,7 +46,7 @@ formats <- xlsx_formats(file.path(dir, filename))
 sector_cell_formats <- xlsx_cells(file.path(dir, filename),
                                   sheets = "Gases",
                                   include_blank_cells = FALSE) %>%
-  filter(col == 3, between(row, 5, 552) | between(row, 525, 601)) %>%
+  filter(between(col,2,3), between(row, 5, 552) | between(row, 525, 601)) %>%
   select(address, row, col, all_sectors = character, local_format_id) %>%
   mutate(
     all_sectors = gsub("^\\s+|\\s+$", "", all_sectors),
@@ -55,9 +55,80 @@ sector_cell_formats <- xlsx_cells(file.path(dir, filename),
     bold = map_lgl(local_format_id, ~ formats$local$font$bold[[.x]]),
     indent = map_int(local_format_id, ~ formats$local$alignment$indent[[.x]]),
     sector_level = case_when(
-      text_colour == "FF00783C" & bold & indent == 0 ~ "sector",
-      !bold & indent == 1 ~ "subsector_level1",
-      text_colour == "FFFFFFFF" & !bold & (indent == 2 | indent ==3) ~ "subsector_level2",
+      is.na(text_colour) & is.na(bg_colour) & bold & indent == 0 ~ "gas",
+      is.na(text_colour) & bg_colour == "FFFFFFFF" & bold & indent == 0 ~ "sector",
+      text_colour == "FF00783C" & bold & indent == 0 ~ "subsector_level1",
+      !bold & indent == 1 ~ "subsector_level2",
+      text_colour == "FFFFFFFF" & !bold & indent == 3 ~ "subsector_level3",
       TRUE ~ "ahhhh"
     )
   )
+
+# Join sector level info with data, filter out total rows
+data_wide <- read_xlsx(file.path(dir, filename),
+                       col_names = newcols,
+                       range = "Economic Sectors!B5:AH51",
+                       na = c("", "-")) %>%
+  mutate(row = seq(5, length.out = nrow(.))) %>%
+  bind_rows(
+    read_xlsx(file.path(dir, filename),
+              col_names = newcols,
+              range = "Economic Sectors!B54:AH63",
+              na = c("", "-")) %>%
+      mutate(row = seq(54, length.out = nrow(.)))
+  ) %>%
+  left_join(
+    sector_cell_formats %>%
+      select(row, all_sectors, sector_level),
+    by = c("row", "all_sectors")
+  ) %>%
+  mutate(all_sectors =  gsub("[0-9]$", "", all_sectors)) %>%
+  pivot_wider(names_from = sector_level, values_from = all_sectors) %>%
+  mutate(
+    subsector_level1 = ifelse(!is.na(sector), "total", subsector_level1),
+    subsector_level2 = ifelse(!is.na(subsector_level1), NA_character_, subsector_level2),
+    sector = ifelse(sector == "OTHER LAND USE", "Other Emissions Not Included In Inventory Total", sector)
+  ) %>%
+  fill(sector, subsector_level1) %>%
+  filter(sector != "total" & subsector_level1 != "total") %>%
+  group_by(sector, subsector_level1) %>%
+  mutate(n = n()) %>%
+  ungroup() %>%
+  filter(n == 1 | (!is.na(subsector_level2) & n > 1)) %>%
+  select(sector, subsector_level1, subsector_level2, `1990`:`2021`)
+
+## Testing to make sure sums are same as input table
+data_long <- data_wide %>%
+  gather(key =  year, value = ktCO2e,
+         -sector, -subsector_level1,
+         -subsector_level2) %>%
+  mutate(ktCO2e = as.numeric(ktCO2e),
+         year = as.integer(as.character(year)))
+
+totals <- data_long %>%
+  filter(sector != "Other Emissions Not Included In Inventory Total") %>%
+  group_by(year) %>%
+  summarise(sum = round(sum(ktCO2e, na.rm=TRUE), digits = 0))
+totals
+
+sector_totals <- data_long %>%
+  filter(sector != "Other Emissions Not Included In Inventory Total") %>%
+  group_by(sector, year) %>%
+  summarise(sum = round(sum(ktCO2e, na.rm=TRUE), digits = 0)) %>%
+  mutate(year = as.integer(as.character(year))) %>%
+  filter(!is.na(year))
+
+sector_totals
+
+## Save the re-formatted data as CSV file
+data_year_range <- range(data_long$year)
+fname <- paste0("bc_ghg_emissions_by_economic_sector_", data_year_range[1], "-", data_year_range[2], ".csv")
+write_csv(data_wide, (file.path(dir, fname)))
+cat(
+  paste0("\n## GHGs by Economic Sector (", fname, ")\n"),
+  replace_na(metadata$Notes, ""),
+  file = file.path(dir, paste0("bc_ghg_emissions_", data_year_range[1], "-", data_year_range[2], "_metadata.txt")),
+  sep = "\n",
+  append = TRUE
+)
+
